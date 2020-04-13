52b7caf273c91de195d33afaee4cd1cdd1bbcc81
Kevin <kevinyilie@gmail.com>
2020-04-13 03:46:08 -0400

    Implement reduce_possible_codes function. Add code parameter to Mastermind class constructor

--- fiveguess.py
+++ fiveguess.py
@@ -6,11 +6,20 @@ Feedback = namedtuple('Feedback', ['blacks', 'whites'])
 
 
 class Mastermind():
-    def __init__(self):
+    def __init__(self, code=None):
         self.pegs = 4
         self.colors = 6
-        self._code = tuple(randint(1, self.colors) for _ in range(self.pegs))
         self.win = False
+
+        if code is None:
+            self._code = tuple(randint(1, self.colors)
+                               for _ in range(self.pegs))
+        else:
+            self._code = code
+
+        assert type(self._code) == tuple
+        assert len(self._code) == self.pegs
+        assert all(n >= 1 and n <= self.colors for n in self._code)
 
     def feedback(self, guess):
         """
@@ -42,20 +51,34 @@ def print_turn(guess, fb):
     print()
 
 
-def remove_codes(poss_codes):
-    pass
+def reduce_possible_codes(possible_codes, guess, fb):
+    """Return a set with all elements from possible_codes that would receive
+    the same feedback as the actual feedback from guess, fb,
+    if guess was the secret code."""
+    new_possible_codes = set()
+    test = Mastermind(guess)
+
+    for code in possible_codes:
+        if test.feedback(code) == fb:
+            new_possible_codes.add(code)
+
+    return new_possible_codes
 
 
-def next_guess(poss_codes):
-    pass
+def next_guess(possible_codes):
+    raise NotImplementedError
 
 
 def main():
+    """
+    Five-guess algorithm steps are directly from the Mastermind wikipedia page:
+    https://en.wikipedia.org/wiki/Mastermind_(board_game)#Five-guess_algorithm
+    """
     mastermind = Mastermind()
 
     # 1. Create the set S of 1296 possible codes
     # (1111, 1112 ... 6665, 6666)
-    poss_codes = set(product([1, 2, 3, 4, 5, 6], repeat=4))
+    possible_codes = set(product([1, 2, 3, 4, 5, 6], repeat=4))
 
     # 2. Start with initial guess 1122
     guess = (1, 1, 2, 2)
@@ -72,10 +95,10 @@ def main():
 
         # 5. Otherwise, remove from S any code that would not give the same
         # response if it (the guess) were the code.
-        poss_codes = remove_codes(poss_codes)
+        possible_codes = reduce_possible_codes(possible_codes, guess, fb)
 
         # 6. Apply minimax technique to find a next guess.
-        guess = next_guess(poss_codes)
+        guess = next_guess(possible_codes)
 
         # 7. Repeat from step 3
 
