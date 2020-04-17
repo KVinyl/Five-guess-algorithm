3c792a866f5547c69a3ec08d8c7ef792799d0f1b
Kevin <kevinyilie@gmail.com>
2020-04-16 22:21:10 -0400

    Add fiveguess_user

---
+++ fiveguess_user.py
@@ -0,0 +1,103 @@
+from collections import Counter, namedtuple
+from itertools import product
+from random import randint
+
+
+init_possible_codes = set(product([1, 2, 3, 4, 5, 6], repeat=4))
+
+Feedback = namedtuple('Feedback', ['blacks', 'whites'])
+
+
+def feedback(code, guess):
+    """
+    Return a namedtuple Feedback(blacks, whites) where
+    blacks is the number of pegs from the guess that
+    are correct in both color and position and
+    whites is the number of pegs of the right color but wrong position.
+    """
+    blacks = sum(g == c for g, c in zip(guess, code))
+    whites = sum((Counter(guess) & Counter(code)).values()) - blacks
+
+    return Feedback(blacks, whites)
+
+
+def turn(guess):
+    """Input feedback of the turn."""
+    print("Guess: ", guess)
+    blacks = int(input("Blacks: "))
+    whites = int(input("Whites: "))
+    print()
+
+    return Feedback(blacks, whites)
+
+
+def reduce_possible_codes(possible_codes, guess, fb):
+    """Return a set with all elements from possible_codes that would receive
+    the same feedback as the actual feedback from guess, fb,
+    if guess was the secret code."""
+
+    return {code for code in possible_codes if feedback(code, guess) == fb}
+
+
+def next_guess(possible_codes, past_guesses):
+    """
+    Return the next guess.
+
+    A score is calculated for each possible guess
+    (any unguessed code in the original 1296 set).
+    The score is the minimum number of possibilites it might
+    eliminate from possible_guesses.
+
+    The minimum eliminated is the count of elements in possible_codes
+    minus the highest hit count (the count of the most frequent black/white peg
+    feedback when passed through possible_codes)
+
+    The next guess is the guess with the highest score and is in possible_set
+    whenever possible.
+    """
+    def score(guess):
+        fbs = [feedback(code, guess) for code in possible_codes]
+        return len(possible_codes) - max(Counter(fbs).values())
+
+    ScoreData = namedtuple('ScoreData', ['score', 'is_possible_code', 'guess'])
+    scores = [ScoreData(score(guess), guess in possible_codes, guess)
+              for guess in (init_possible_codes - past_guesses)]
+
+    return max(scores).guess
+
+
+def main():
+    """
+    Five-guess algorithm steps are directly from the Mastermind wikipedia page:
+    https://en.wikipedia.org/wiki/Mastermind_(board_game)#Five-guess_algorithm
+    """
+    # 1. Create the set S of 1296 possible codes
+    # (1111, 1112 ... 6665, 6666)
+    possible_codes = init_possible_codes.copy()
+    guesses = set()
+
+    # 2. Start with initial guess 1122
+    guess = (1, 1, 2, 2)
+
+    while True:
+        # 3. Play the guess to get a response of coloured and white pegs.
+        guesses.add(guess)
+        fb = turn(guess)
+
+        # 4. If the response is four colored pegs,
+        # the game is won, the algorithm terminates.
+        if fb.blacks == 4:
+            break
+
+        # 5. Otherwise, remove from S any code that would not give the same
+        # response if it (the guess) were the code.
+        possible_codes = reduce_possible_codes(possible_codes, guess, fb)
+
+        # 6. Apply minimax technique to find a next guess.
+        guess = next_guess(possible_codes, guesses)
+
+        # 7. Repeat from step 3
+
+
+if __name__ == "__main__":
+    main()
